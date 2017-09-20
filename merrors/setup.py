d94e19a9115ae55209808c804cc737dd4570422d
Zack P <zack@zacklearns.com>
2017-09-20 15:27:31 -0600

    Create setup.py

---
+++ merrors/setup.py
@@ -0,0 +1,24 @@
+# Setup the Miniblast installer.
+
+from distutils.core import setup
+
+setup(
+  name="merror",
+  version="1.0.0",
+  description="The Python framework that gives you a blast.  Error decoder.",
+  author="Zack Pace",
+  email="zack@zacklearns.ca",
+  url="https://github.com/miniblast",
+  packages=["merrors"]
+  classifiers=[
+    "Development Status :: 1 - Pre-alpha",
+    "Enviroment :: Web Enviroment",
+    "Enviroment :: Console",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independant",
+    "Natural Langauge :: English",
+    "Programming Language :: Python",
+    "Programming Language :: Ruby",
+    "Topic :: Internet",
+  ]
+)
