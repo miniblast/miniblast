34bfb6b96ba8671c54f2aec4b8ddbc3249e419b7
Zack P <zack@zacklearns.com>
2017-09-22 10:57:25 -0600

    Update supportlibrary.rb

--- supportlibrary/supportlibrary.rb
+++ supportlibrary/supportlibrary.rb
@@ -4,4 +4,12 @@ module SUPPORT
   def self.GetCurrentVersion()
     return "1.1.2"
   end
+  
+  def self.GetCurrentConfigs()
+    return Gem::Blast::bml
+  end
+  
+  def self.ReturnCurrentUpdate()
+    return "Miniblast1 :: Ruby 2.4.2 or Greater."
+  end
 end
