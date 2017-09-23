8ef4266f96e83e667ab7562595994e66f41bf01b
Zack P <zack@zacklearns.com>
2017-09-23 17:25:42 -0600

    Update miniblast.rb

--- lib/miniblast.rb
+++ lib/miniblast.rb
@@ -1,11 +1,21 @@
 require "rulers/version"
-
-module Miniblast
+require "rulers/routing"
+module Rulers
 class Application
    def call(env)
-     `echo debug > debug.txt`;
+     klass, act = get_controller_and_action(env)
+     controller = klass.new(env)
+     text = controller.send(act)
      [200, {'Content-Type' => 'text/html'},
-      ["Hello from Miniblast!"]]
+ [text]]
+   end
+end
+class Controller
+   def initialize(env)
+     @env = env
+   end
+   def env
+     @env
    end
 end
 end
