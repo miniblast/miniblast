804a4429c5507a62f6771f9c6e749859a42d8210
Zack P <zack@zacklearns.com>
2017-09-24 10:59:55 -0600

    Create application.rb

---
+++ config/application.rb
@@ -0,0 +1,9 @@
+require "rulers"
+module BestQuotes
+class Application < Rulers::Application
+end
+end
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+ "..", "app",
+                       "controllers")
+require "quotes_controller"
