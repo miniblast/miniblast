5a5a4a2454b228a4f58b1adabfc14fc455613fc5
Zack P <zack@zacklearns.com>
2017-09-24 11:04:57 -0600

    Delete application.rb

--- config/application.rb
+++
@@ -1,9 +0,0 @@
-require "rulers"
-module BestQuotes
-class Application < Rulers::Application
-end
-end
-$LOAD_PATH << File.join(File.dirname(__FILE__),
- "..", "app",
-                       "controllers")
-require "quotes_controller"
